50eaedf342540734c1074967432b432a624d9875
sub6550 <sub6550@gmail.com>
2025-09-26 17:51:30 +0900

    ch09 Github Action

--- ch09/src/main/java/kr/co/ch09/security/SecurityConfig.java
+++ ch09/src/main/java/kr/co/ch09/security/SecurityConfig.java
@@ -1,34 +1,35 @@
 package kr.co.ch09.security;
 
+import kr.co.ch09.jwt.JwtAuthenticationFilter;
+import kr.co.ch09.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
 
+    private final JwtProvider jwtProvider;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        // 로그인 설정
-        http.formLogin(form -> form
-                .loginPage("/user/login")
-                .defaultSuccessUrl("/")
-                .failureUrl("/user/login?error=true")
-                .usernameParameter("usid")
-                .passwordParameter("pass")
-        );
-
-        // 로그아웃 설정
-        http.logout(logout -> logout
-                .logoutUrl("/user/logout")
-                .invalidateHttpSession(true)
-                .logoutSuccessUrl("/user/login?logout=true"));
+        http.httpBasic(HttpBasicConfigurer::disable)        // 기본 HTTP 인증 비활성
+                .formLogin(FormLoginConfigurer::disable)    // 폼 로그인 비활성
+                .sessionManagement(config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 비활성
+                .addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class);
 
         // 인가 설정
         http.authorizeHttpRequests(authorize -> authorize
@@ -36,6 +37,7 @@ public class SecurityConfig {
                 .requestMatchers("/manager/**").hasAnyRole("ADMIN", "MANAGER")
                 .requestMatchers("/member/**").hasAnyRole("ADMIN", "MANAGER", "MEMBER")
                 .requestMatchers("/guest/**").permitAll()
+                .requestMatchers("/user/**").hasAnyRole("ADMIN", "MANAGER")
                 .anyRequest().permitAll()
         );
 
@@ -50,5 +52,35 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
